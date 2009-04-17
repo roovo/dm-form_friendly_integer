178b92ae2d1235ad841830e8157c6cc9187f0535
Rupert Voelcker <rupert@rupespad.com>
2009-04-17 23:00:25 +0100

    added implementation

--- lib/dm-types/form_friendly_integer.rb
+++ lib/dm-types/form_friendly_integer.rb
@@ -2,7 +2,25 @@ module DataMapper
   module Types
     class FormFriendlyInteger < DataMapper::Type
       primitive Integer
-   end
+
+      def self.load(value, property)
+        if value.is_a?(Integer) || value.nil?
+          value
+        else
+          raise ArgumentError.new("+value+ must be nil or a String")
+        end
+      end
+
+      def self.typecast(value, property)
+        if value.kind_of?(String) && value =~ /\A[+-]?\d+\z/
+          value.to_i
+        elsif value.kind_of?(String)
+          value
+        else
+          load(value, property)
+        end
+      end
+    end
   end
 end
 
